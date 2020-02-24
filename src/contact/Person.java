a6da79b837bcf9acb33d5e269d90ab32dc31c435
shubhanjayt <shubhanjay.tiwari_iot18@gla.ac.in>
2020-02-24 18:48:11 +0530

    getter and setter created for the variables in the person class

--- src/contact/Person.java
+++ src/contact/Person.java
@@ -15,4 +15,52 @@ public class Person implements Serializable {
     public Person() {
         next = null;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getAnotherContactNumber() {
+        return anotherContactNumber;
+    }
+
+    public void setAnotherContactNumber(String anotherContactNumber) {
+        this.anotherContactNumber = anotherContactNumber;
+    }
+
+    public String getE_mailAddress() {
+        return e_mailAddress;
+    }
+
+    public void setE_mailAddress(String e_mailAddress) {
+        this.e_mailAddress = e_mailAddress;
+    }
+
+    public Person getNext() {
+        return next;
+    }
+
+    public void setNext(Person next) {
+        this.next = next;
+    }
 }
